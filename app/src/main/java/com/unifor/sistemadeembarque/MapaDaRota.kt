9982a1b4acca39dfa7155d8cf855e36ccc70363b
Davi Camara <davirego9.dc@gmail.com>
2021-11-23 00:22:49 -0300

    Finalização do modulo ode GPS, agora o ponto representado no mapa, condiz com a localização atual do usuário, deletado classes usadas de exemplo para puxar coordenadas.

--- app/src/main/java/com/unifor/sistemadeembarque/MapaDaRota.kt
+++ app/src/main/java/com/unifor/sistemadeembarque/MapaDaRota.kt
@@ -1,22 +1,31 @@
 
 package com.unifor.sistemadeembarque
 
+import android.location.Location
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-    class MapaDaRota : AppCompatActivity(), OnMapReadyCallback {
+
+    class MapaDaRota : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMarkerClickListener {
+        private lateinit var map: GoogleMap
+        private lateinit var fusedLocationClient: FusedLocationProviderClient
+        private lateinit var lastLocation: Location
 
         // [START_EXCLUDE]
         // [START maps_marker_get_map_async]
         override fun onCreate(savedInstanceState: Bundle?) {
+
             super.onCreate(savedInstanceState)
             // Retrieve the content view that renders the map.
             setContentView(R.layout.activity_mapa_da_rota)
@@ -24,22 +33,54 @@ import com.google.android.gms.maps.model.MarkerOptions
             // Get the SupportMapFragment and request notification when the map is ready to be used.
             val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
             mapFragment?.getMapAsync(this)
+
+
+
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         }
         // [END maps_marker_get_map_async]
         // [END_EXCLUDE]
 
         // [START maps_marker_on_map_ready_add_marker]
         override fun onMapReady(googleMap: GoogleMap) {
-            val sydney = LatLng(-33.852, 151.211)
-            googleMap.addMarker(
-                MarkerOptions()
-                    .position(sydney)
-                    .title("Marker in Sydney")
-            )
-            // [START_EXCLUDE silent]
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            map = googleMap
+            map.uiSettings.isZoomControlsEnabled = true
+
+            /*map.setOnMarkerClickListener(this)*/
+            map.setOnMarkerClickListener { marker ->
+                if (marker.isInfoWindowShown) {
+                    marker.hideInfoWindow()
+                } else {
+                    marker.showInfoWindow()
+                }
+                true
+            }
+
+
+            setUpMap()
             // [END_EXCLUDE]
         }
+
+        private fun setUpMap() {
+            map.isMyLocationEnabled = true
+
+// 2
+            fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+                // Got last known location. In some rare situations this can be null.
+                // 3
+                if (location != null) {
+                    lastLocation = location
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                }
+            }
+        }
+
+        override fun onMarkerClick(p0: Marker): Boolean {
+            TODO("Not yet implemented")
+        }
+
+
         // [END maps_marker_on_map_ready_add_marker]
     }
 // [END maps_marker_on_map_ready]
